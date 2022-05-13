6055b3b5d2cf030a5007e886b91bee84f288c7c1
Jack Kingham <wowknead@gmail.com>
2022-05-12 22:20:28 -0400

    Added comments to reference.rs

--- reference.rs
+++ reference.rs
@@ -3,9 +3,12 @@ fn main() {
 
     foo(&s1);
 
+    //s1 could no longer be referenced here if not passed as reference
     println!("s1 is still valid, see: \"{s1}\"");
 }
 
+//If s was not a reference, s would change ownership
+//and its scope would end at the end of foo
 fn foo(s: &String) {
     println!("The value of s is \"{s}\"");
 }
