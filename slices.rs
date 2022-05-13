21ac73f6931398d36ba639f3d2c0c443f7bb53a9
Jack Kingham <wowknead@gmail.com>
2022-05-12 23:16:52 -0400

    Created slices demonstration. Nothing in it yet though.

---
+++ slices.rs
@@ -0,0 +1,7 @@
+//Slices let you reference a contiguous sequence of elements in
+//a collection rather than the whole collection.
+//A slice is a kind of reference, so it does not have ownership.
+
+fn main() {
+
+}
