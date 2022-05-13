c528c9e2c8a269a43fbd514a7982cb724a8563c1
Jack Kingham <wowknead@gmail.com>
2022-05-12 22:41:38 -0400

    Lots of explanation about mut refs.

--- mut_reference.rs
+++ mut_reference.rs
@@ -1,3 +1,9 @@
+fn foo(s: &mut String) {
+    //    ^^^^ <-- mut reference applied to type
+    println!("Changing s1 from foo!");
+    s.push_str(" hi!");
+}
+
 fn main() {
     let mut s1 = String::from("I am string.");
     //  ^^^ <-- s1 must be mut in the first place
@@ -6,16 +12,22 @@ fn main() {
 
     println!("{s1}");
 
-    //NOTE THAT TWO MUT REFS CANNOT BE CREATED FOR THE SAME VARIABLE
-    //ONE MUT REF PER VARIABLE
+    //NOTE THAT TWO MUT REFS CANNOT BE CREATED FOR THE SAME VARIABLE 
+    //ONE MUT REF PER VARIABLE IN THE SAME SCOPE
     //This does not work:
     //  let s1_mutref1 = &mut s1;
     //  let s1_mutref2 = &mut s1;
-    //  println!("{s1_mutref1} {s1_mutref2}");
-}
 
-fn foo(s: &mut String) {
-    //    ^^^^ <-- mut reference applied to type
-    println!("Changing s1 from foo!");
-    s.push_str(" hi!");
+    //Instead use curly brackets to limit scope like this:
+    //  { let s1_mutref1 = &mut s1; }
+    //  let s1_mutref2 = &mut s1;
+
+    //Additionally, mutable references cannot exist in
+    //the same scope as immutable ones.
+    //This does not work:
+    //  let s1_ref = &s1;
+    //  let s1_mutref = &mut s1;
+
+    //As many immutable references as you want can be created
+    //because this has no datarace potential.
 }
