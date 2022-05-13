10f34b5c52dff688f8d8fa88e2e5b72f67a6adf4
Jack Kingham <wowknead@gmail.com>
2022-05-12 22:22:19 -0400

    Even more comments in reference.

--- reference.rs
+++ reference.rs
@@ -2,6 +2,7 @@ fn main() {
     let s1 = String::from("I am string.");
 
     foo(&s1);
+    //  ^ <-- & must be here for reference to s1
 
     //s1 could no longer be referenced here if not passed as reference
     println!("s1 is still valid, see: \"{s1}\"");
@@ -10,5 +11,6 @@ fn main() {
 //If s was not a reference, s would change ownership
 //and its scope would end at the end of foo
 fn foo(s: &String) {
+    //    ^ <-- Note that & is applied to the type
     println!("The value of s is \"{s}\"");
 }
