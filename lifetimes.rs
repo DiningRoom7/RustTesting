ef21dc56ee1a41aeef89a8a5016a4d0e3f7b19f4
Jack Kingham <wowknead@gmail.com>
2022-06-13 18:39:18 -0400

    Fixed the lifetimes demonstration.

--- lifetimes.rs
+++ lifetimes.rs
@@ -18,8 +18,9 @@ fn main() {
 //Error message: "this function's return type contains a borrowed value,
 //                but the signature does not say whether it is borrowed 
 //                from `s1` or `s2`"
-fn longest(s1: & str, s2: & str) -> & str {
+fn longest<'a>(s1: &'a str, s2: &'a str) -> &'a str {
     //    ^^^^------^^-----------^^----------^^---> These lifetime specifications must be added
-    //This just means it will find some lifetime such that s1 and s2 will live at least that long]
+    //This just means it will find some lifetime such that s1 and s2 will live at least that long
+    //Note that the lifetime of the return value must be the same as one of the parameters
     if s1.len() > s2.len() { s1 } else { s2 }
 }
