1738439457c61d57988a2f0f94bc14a52ce5a239
Jack Kingham <wowknead@gmail.com>
2022-06-13 20:55:43 -0400

    Added a demonstration of traits.

---
+++ traits.rs
@@ -0,0 +1,32 @@
+//Defining a trait Summary with one method summarize
+trait Summary {
+    fn summarize(&self) -> &str;
+}
+
+//Some traits such as Debug, Ord, and Clone can be automatically implemented using
+#[derive(Debug)]
+struct Article {
+    content: String
+}
+
+//Implementing the methods of the Summary trait for our Article struct
+impl Summary for Article {
+    fn summarize(&self) -> &str {
+        for (i, c) in self.content.chars().enumerate() {
+            if c == ' ' { return &self.content[..i]}
+        }
+        &self.content[..]
+    }
+}
+
+
+
+fn main() {
+    let a = Article { content: String::from("Hello there.")};
+
+    //print the summary
+    println!("{}", a.summarize());
+
+    //print the debug output
+    println!("{:?}", a);
+}
